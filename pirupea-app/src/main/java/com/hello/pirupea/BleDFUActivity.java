6c63b46d4f17cf3f67a7ea1193fa9ae41bb2c5bd
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2014-09-12 11:44:41 -0700

    Added basic buttons

--- pirupea-app/src/main/java/com/hello/pirupea/BleDFUActivity.java
+++ pirupea-app/src/main/java/com/hello/pirupea/BleDFUActivity.java
@@ -1,18 +1,68 @@
 package com.hello.pirupea;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class BleDFUActivity extends Activity {
+public class BleDFUActivity extends Activity{
+    static final int REQUEST_ENABLE_BT = 2;
+    private Button btnDFUScan;
+    private BluetoothDevice dfuTarget;
 
+
+    private void showToast(final String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    private void showToast(final int messageResId) {
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+    private void isBLESupported() {
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            showToast(R.string.no_ble);
+            finish();
+        }
+    }
+    private boolean isBLEEnabled() {
+        final BluetoothManager manager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        final BluetoothAdapter adapter = manager.getAdapter();
+        return adapter != null && adapter.isEnabled();
+    }
+
+    private void showBLEDialog() {
+        final Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ble_dfu);
+        isBLESupported();
+        if(!isBLEEnabled()){
+            showBLEDialog();
+        }
+
+        this.btnDFUScan = (Button) findViewById(R.id.btnDFUScan);
+        this.btnDFUScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                Log.i("DFU", "Scan Button Pressed");
+            }
+        });
     }
+
+
 
 
     @Override
